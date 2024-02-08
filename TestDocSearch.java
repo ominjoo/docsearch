5ceb11db2480a223592a61de0db19f4cb24dbf78
ominjoo <o.minjoo14@gmail.com>
2024-02-07 20:34:58 -0800

    commit

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -18,5 +18,13 @@ public class TestDocSearch {
     String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
     assertEquals(expect, h.handleRequest(rootPath));
 	}
+
+    @Test
+    public void testSearchWord() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/biomed/");
+        URI rootPath = new URI("http://localhost/search?q=reptiles");
+        String expect = "Found 1 paths:\n./technical/biomed/gb-2003-4-3-r18.txt";
+        assertEquals(expect, h.handleRequest(rootPath));
+        }
 }
 
